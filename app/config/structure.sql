738c54289eff6839e3dd74aa38ccf49472f651af
ConstantineUA <eichegarten@yandex.ru>
2015-09-24 00:09:54 +0300

    Improve SQL-structure dump

--- app/config/structure.sql
+++ app/config/structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 23, 2015 at 11:49 PM
+-- Generation Time: Sep 24, 2015 at 12:08 AM
 -- Server version: 5.5.43-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.9
 
@@ -22,7 +22,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `name` varchar(64) NOT NULL,
   `type` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table to store payment categories' AUTO_INCREMENT=19 ;
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `payments` (
   `category_id` int(11) NOT NULL,
   `amount` decimal(15,2) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `comment` varchar(512) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `comment` varchar(512) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Table to store payments' AUTO_INCREMENT=31 ;
