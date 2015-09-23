8ccfc49f7321fcda4e1a7f38d385c808fe47e8ff
ConstantineUA <eichegarten@yandex.ru>
2015-09-24 00:07:16 +0300

    Remove debug-related instructions from the index.php

--- web/index.php
+++ web/index.php
@@ -3,11 +3,6 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-ini_set('display_errors', 'On');
-error_reporting(-1);
-date_default_timezone_set('Europe/Kiev');
-
-
 $app = new Accountant\Application(
     require __DIR__ . '/../app/config/dev.php'
 );
